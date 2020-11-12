345c81e27a0f18b7feaae2d43ec590aac72a80b9
MichaelPrigmore <prig1103@stthomas.edu>
2020-11-11 19:03:09 -0600

    failed

---
+++ SkunkProject/test/skunk/domain/Die_Testable_Tests.java
@@ -0,0 +1,27 @@
+package skunk.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Die_Testable_Tests
+{
+
+	@Test
+	public void test_die_testable_constructor()
+	{
+		fail();
+		// Die myDie = new Die();
+		// assertNotNull(myDie);
+	}
+	//
+	// @Test
+	// public void test_override_toString_die()
+	// {
+	// Die myDie = new Die();
+	// assertEquals(myDie.toString().charAt(0), 'D');
+	// }
+
+}
