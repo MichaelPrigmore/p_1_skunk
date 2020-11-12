0886ff80fe3b3a6fb3b5fd51471a1294e97d3d81
MichaelPrigmore <prig1103@stthomas.edu>
2020-11-11 20:01:52 -0600

    failed

--- SkunkProject/test/skunk/domain/Dice_Modular_Tests.java
+++ SkunkProject/test/skunk/domain/Dice_Modular_Tests.java
@@ -1,6 +1,7 @@
 package skunk.domain;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,41 @@ class Dice_Modular_Tests
 {
 
 	@Test
-	void test()
+	public void test_dice_modular_constructor()
 	{
-		fail("Not yet implemented");
+		// Die_Modular myDie = new Die_Modular();
+		// assertNotNull(myDie);
+		fail();
 	}
+
+	// @Test
+	// public void test_override_toString_die_testable()
+	// {
+	// Die_Modular myDie = new Die_Modular();
+	// assertEquals(myDie.toString().charAt(0), 'D');
+	// }
+	//
+	// @Test
+	// public void test_500_fair_roll_values()
+	// {
+	// Die_Modular myDie = new Die_Modular();
+	//
+	// for (int i = 0; i < 500; i++)
+	// {
+	// myDie.roll();
+	// assertTrue(myDie.getLastRoll() > 0 && myDie.getLastRoll() < 7);
+	// }
+	// }
+	//
+	// @Test
+	// public void test_unfair_roll_value()
+	// {
+	// Die_Modular myDie = new Die_Modular();
+	// myDie.setIsFairDie(false);
+	// myDie.setLastRoll(1);
+	// myDie.roll();
+	// assertEquals(myDie.getLastRoll(), 2);
+	//
+	// }
 
 }
