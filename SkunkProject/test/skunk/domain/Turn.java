3d0f9a45fca39c06c58674ffa3046db69be77b87
MichaelPrigmore <prig1103@stthomas.edu>
2020-11-18 17:55:16 -0600

    passed

--- SkunkProject/test/skunk/domain/Turn.java
+++ SkunkProject/test/skunk/domain/Turn.java
@@ -3,21 +3,28 @@ package skunk.domain;
 public class Turn
 {
 	private Player player;
+	private int Current_Turn_Score;
 
 	public Turn()
 	{
-
+		this.Current_Turn_Score = 0;
 	}
 
 	public void setPlayer(Player player)
 	{
 		this.player = player;
+		this.Current_Turn_Score = 0;
 
 	}
 
 	public Object getPlayer()
 	{
 		return this.player;
+	}
+
+	public int get_Current_Turn_Score()
+	{
+		return this.Current_Turn_Score;
 	}
 
 }
