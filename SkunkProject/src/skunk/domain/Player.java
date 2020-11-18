b839dd55aafddd1f154611da84c002ad1e9a4eb5
MichaelPrigmore <prig1103@stthomas.edu>
2020-11-18 17:31:08 -0600

    passed

--- SkunkProject/src/skunk/domain/Player.java
+++ SkunkProject/src/skunk/domain/Player.java
@@ -4,11 +4,19 @@ public class Player
 {
 	private int score;
 	private int chips;
+	private String name;
 
 	public Player()
 	{
 		this.score = 0;
 		this.chips = 50;
+		this.name = "default";
 	}
 
+	public Player(String name)
+	{
+		this.score = 0;
+		this.chips = 50;
+		this.name = name;
+	}
 }
