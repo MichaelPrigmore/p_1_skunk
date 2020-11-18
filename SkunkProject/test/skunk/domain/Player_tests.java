3c8111e93c70cd08ed6dcc04e89be25527dbf72e
MichaelPrigmore <prig1103@stthomas.edu>
2020-11-18 17:27:58 -0600

    test first player constructor - failed

---
+++ SkunkProject/test/skunk/domain/Player_tests.java
@@ -0,0 +1,17 @@
+package skunk.domain;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Player_tests
+{
+
+	@Test
+	public void test_player_first_constructor()
+	{
+		fail();
+	}
+
+}
